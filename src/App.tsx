bae67c18bf7a4c73796ce6a4e15d44634588917b
elad privet <eladprivet@elads-MBP.Dlink>
2024-02-10 19:19:03 +0200

    made the note form and the add new note

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,75 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import NewNotes from "./NewNotes";
+import { useLocalStorage } from "./useLocalStorage";
+import { useMemo } from "react";
+import { v4 as uuidV4 } from "uuid";
+
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type RawNote = {
+  id: string;
+} & RawNoteData;
+
+export type RawNoteData = {
+  title: string;
+  markdown: string;
+  tagIds: string[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
 
 function App() {
+  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", []);
+  const [tags, settags] = useLocalStorage<Tag[]>("TAGS", []);
+
+  const notesWithTags = useMemo(() => {
+    return notes.map((note) => {
+      return {
+        ...note,
+        tags: tags.filter((tag) => note.tagIds.includes(tag.id)),
+      };
+    });
+  }, [notes, tags]);
+
+  function onCreateNote({ tags, ...data }: NoteData) {
+    setNotes((prevNotes) => {
+      return [
+        ...prevNotes,
+        { ...data, id: uuidV4(), tagIds: tags.map((tag) => tag.id) },
+      ];
+    });
+  }
+
+  function addTagt(tag: Tag) {
+    settags((prev) => [...prev, tag]);
+  }
+
   return (
     <Container className="my-4">
       <Routes>
         <Route path="/" element={<h1>home</h1>} />
-        <Route path="/new" element={<NewNotes />} />
+        <Route
+          path="/new"
+          element={
+            <NewNotes
+              onSubmit={onCreateNote}
+              onAddTag={addTagt}
+              availavleTags={tags}
+            />
+          }
+        />
         <Route path="/:id">
           <Route index element={<h1>show</h1>} />
           <Route path="edit" element={<h1>edit</h1>} />
--- src/NewNotes.tsx
+++ src/NewNotes.tsx
@@ -1,10 +1,25 @@
+import { NoteData, Tag } from "./App";
 import NoteForm from "./NoteForm";
 
-export default function NewNotes() {
+type NewNotesProps = {
+  onSubmit: (data: NoteData) => void;
+  onAddTag: (tag: Tag) => void;
+  availavleTags: Tag[];
+};
+
+export default function NewNotes({
+  onSubmit,
+  onAddTag,
+  availavleTags,
+}: NewNotesProps) {
   return (
     <>
       <h1 className="mb-4">NewNotes</h1>
-      <NoteForm />
+      <NoteForm
+        onSubmit={onSubmit}
+        onAddTag={onAddTag}
+        availavleTags={availavleTags}
+      />
     </>
   );
 }
